93552c15e064f4985bbc8d4a7973434bccbdcce1
YATC356 <72997986@CERTUS.EDU.PE>
2026-09-14 22:49:25 -0500

    practica

---
+++ ejemplo8.py
@@ -0,0 +1,15 @@
+#Teniendo un csv con nombre, apellido y edad; encontrar la edad de algun miembro de la lista
+
+#Abri el archivo
+
+#imprime el archivo
+
+# mientras (WHILE) no se llega al final del archivo (EOF) hacer:
+    #partir(split) la linea segun el marcador ";"
+          #if subcadena = "nombreElegido" THEN imprime subcadena1
+
+#cerrar el archivo
+
+
+
+
